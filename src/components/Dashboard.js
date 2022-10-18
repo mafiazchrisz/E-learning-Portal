8b1d537ec27a179bb15e1a6913fbb34a281b6256
ChrisZenos <wasin_jongangjit@cmu.ac.th>
2022-10-19 01:20:42 +0700

    Initial

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,9 +6,12 @@ function Dashboard() {
     <div className="container-box">
         <div className="container-header">Dashboard</div>
             <div className="platform">
-                <a href="https://www.microsoft.com/en/microsoft-teams/log-in" target="_blank" className="Team">Microsoft Team</a>
-                <a href="https://mango.cmu.ac.th/" target="_blank" className="Mango">Mango CMU</a>
-                <a href="https://elearning.cmu.ac.th/login/index.php" target="_blank" className="Moodle">KC-Moodle CMU</a>
+                <a href="https://www.microsoft.com/en/microsoft-teams/log-in" target="_blank" className="Team">
+                <img class="img-team" src="images/team.png" /> Microsoft Team</a>
+                <a href="https://mango.cmu.ac.th/" target="_blank" className="Mango">
+                <img class="img-team" src="images/mango.png"/> Mango CMU</a>
+                <a href="https://elearning.cmu.ac.th/login/index.php" target="_blank" className="Moodle">
+                <img class="img-team" src="images/moodle.png"/> KC-Moodle CMU</a>
             </div>    
     </div>
   )
